8c05ad8c027560044a9f4c8ae77e1936c7ea0816
Martin Walker <martin@marsjupiter.com>
2022-04-08 16:02:53 +0100

    can get an auth token

--- auth.js
+++ auth.js
@@ -11,7 +11,8 @@ module.exports = function(RED) {
         this.topics = {};
 
         this.on("input", function(msg) {
-                var axios = require("axios")
+                var axios = require("axios");
+                var crypto = require("crypto");
                 const signUrl = '/v1.0/token?grant_type=1';
                 const timestamp = Date.now().toString();
                 var input = Number(msg.payload);
@@ -21,10 +22,6 @@ module.exports = function(RED) {
                 }
                 var url = host+signUrl;
 
-                var accessKey="";
-                if (msg.hasOwnProperty("accessKey")){
-                  accessKey = msg.accessKey;
-                }
                 var secretKey="";
                 if (msg.hasOwnProperty("secretKey")){
                   secretKey = msg.secretKey;
@@ -34,37 +31,37 @@ module.exports = function(RED) {
                     clientKey = msg.clientKey;
                 }
                 
-                var answer = getRequestSign(timestamp,clientKey,accessKey,secretKey,url, method, {}, "");
-               
-               
-                const contentHash = crypto.createHash('sha256').update('').digest('hex');
-                const stringToSign = [method, contentHash, '', signUrl].join('\n');
-                const signStr = config.accessKey + timestamp + stringToSign;
-              
+                var answer = getRequestAuth(timestamp,clientKey,secretKey,signUrl, "GET", {}, "");
+                msg.signStr = answer.signStr;
+                msg.sortedQuery = answer.sortedQuery;
                 const headers = {
                   t: timestamp,
                   sign_method: 'HMAC-SHA256',
-                  client_id: accessKey,
-                  sign: await encryptStr(signStr, secretKey),
+                  client_id: clientKey,
+                  sign:  answer.sign
                 };
-
-                httpClient = axios.create({
+                msg.headers = headers;
+                var httpClient = axios.create({
                     baseURL: host,
                     timeout: 5 * 1e3,
                   });
-                const { data: login } = await httpClient.get(signUrl, { headers });
-                if (!login || !login.success) {
-                  throw Error(`Authorization Failed: ${login.msg}`);
-                }
-                msg.payload = login.result;
+                //node.warn(host + " > "+httpClient);  
+                httpClient.get(signUrl, { headers }).then(res => {
+                  
+                    msg.payload = res.data;
                 
-                node.send(msg);
-
-            
+                    node.send(msg);
+                  })
+                  .catch(error => {
+                    
+                    msg.payload = error;
+                    node.send(msg)
+                  })
+              
         });
     }
 
-    RED.nodes.registerType("tuya_sign", tuya_auth);
+    RED.nodes.registerType("tuya_auth", tuya_auth);
 
     //var axios = require("axios");
     function tuya_sign(config) {
@@ -147,10 +144,42 @@ function getRequestSign( t,clientKey,accessKey,secretKey, path,  method,  header
   const stringToSign = [method, contentHash, '', url].join('\n');
   const signStr = clientKey + accessKey + t + stringToSign;
   return  {
-      sign: encryptStr(signStr, secretKey)
-      //querystring: querystring,
-      //sortedQuery: sortedQuery
+      sign: encryptStr(signStr, secretKey),
+      signStr: signStr,
+      sortedQuery: sortedQuery
   }
 
 }
 
+/**
+ * Request signature, which can be passed as headers
+ * @param path
+ * @param method
+ * @param headers
+ * @param body
+ */
+ function getRequestAuth( t,clientKey,secretKey, path,  method,  headers,  body) {
+    var crypto = require("crypto");
+    var qs = require("qs");
+    
+    const [uri, pathQuery] = path.split('?');
+    const queryMerged =  qs.parse(pathQuery);
+    var sortedQuery= {};
+    Object.keys(queryMerged)
+      .sort()
+      .forEach((i) => (sortedQuery[i] = queryMerged[i]));
+  
+    const querystring = decodeURIComponent(qs.stringify(sortedQuery));
+    
+    const url = querystring ? `${uri}?${querystring}` : uri;
+    //const contentHash = crypto.createHash('sha256').update(JSON.stringify(body)).digest('hex');
+    const contentHash = crypto.createHash('sha256').update(body).digest('hex');
+    const stringToSign = [method, contentHash, '', url].join('\n');
+    const signStr = clientKey  + t + stringToSign;
+    return  {
+        sign: encryptStr(signStr, secretKey),
+        signStr: signStr,
+        sortedQuery: sortedQuery
+    }
+  
+  }
